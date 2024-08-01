533f4c59f0865aa67bc7f024cb6cf2448750a35c
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2024-08-02 03:04:02 +0900

    [Silver I] Title: 회전 초밥, Time: 1148 ms, Memory: 18600 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+
+public class Main{
+    static int jubsi, chobab, conti, c,max = 0, selec[],x[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        jubsi = Integer.parseInt(st.nextToken());
+        chobab = Integer.parseInt(st.nextToken());
+        conti = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        x = new int[jubsi];
+        selec = new int[chobab+1];
+
+        for(int  i =0 ;i<jubsi;i++){
+            x[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 0 ;i<chobab+1;i++){
+            selec[i] = 300001;
+        }
+
+        dfs(0);
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+    }
+
+    public static void dfs(int depth){
+
+        for(int  i = 0; i<jubsi;i++){
+            int cnt = 0, flag= 0;
+
+            for(int j = i; j < i+conti;j++){
+                if(selec[x[j % jubsi]] != i){
+                    cnt++;
+                }
+                selec[x[j % jubsi]] = i;
+
+                if(x[j % jubsi] == c){
+                    flag = 1;
+                }
+
+            }
+
+            if(flag != 1){
+                cnt++;
+            }
+
+            max = Math.max(max,cnt);
+
+
+        }
+
+    }
+
+}
