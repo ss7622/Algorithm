0995a49cb7dd940cf252302195c901432145fd62
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2024-08-01 17:16:40 +0900

    [Silver I] Title: 기타 레슨, Time: 340 ms, Memory: 23544 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -4,7 +4,7 @@ import java.math.*;
 import java.lang.*;
 
 public class Main{
-    static int sum=0, N, M,x[], start=0, min =2147482900;
+    static int sum=0, N, M,x[], min =2147000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -18,51 +18,47 @@ public class Main{
 
         x = new int[N];
 
-        int s = 0;
-        int m = 0;
+        int s = 0, m=0;
+
         for(int i = 0 ;i<N;i++){
             x[i] = Integer.parseInt(st.nextToken());
             s += x[i];
             m = Math.max(m,x[i]);
         }
 
-        start = s/M;
-        
-        if(start < m){
-            start = m;
-        }
-
-        dfs(start);
+        binary(m,s+1);
 
         bw.write(String.valueOf(min));
         bw.flush();
 
     }
 
-    public static void dfs(int depth){
+    public static void binary(int left,int right){
+        int mid  = (left + right) / 2;
 
-        int cnt = 0;
-        int flag = 0;
-        sum = 0;
-
-        for(int i=0;i<N;i++){
-            sum += x[i];
-            if(sum > depth){
-                cnt++;
-                sum = x[i];
-            }
-
-            if(cnt == M){
-                dfs(depth+1);
-                flag = 1;
-                break;
-            }
+        if(left >= right){
+            return;
         }
 
-        if(flag == 0) {
-            if (min > depth) {
-                min = depth;
+        int s = 0, cnt = 1;
+
+        for(int  i = 0;i<N;i++){
+            s += x[i];
+
+            if(s > mid){
+                cnt++;
+                s = x[i];
             }
+        }
+        if(cnt <= M){
+            binary(left, mid);
+        }
+        if(cnt > M){
+            binary(mid+1, right);
+        }
+
+        if(cnt <= M){
+            min = Math.min(min, mid);
         }
 
     }
