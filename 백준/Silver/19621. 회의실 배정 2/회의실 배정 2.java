41fd22fe9cf93f72bb8a298617c96d46d37c781b
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2024-08-18 20:27:56 +0900

    [Silver II] Title: 회의실 배정 2, Time: 136 ms, Memory: 14560 KB -BaekjoonHub

---
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,x[][], max = 0;
+    static boolean visited[];
+
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+
+        x = new int[N][3];
+        visited = new boolean[N];
+
+        for(int  i = 0 ;i<N;i++){
+            StringTokenizer st  = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+
+            x[i][0] = start;
+            x[i][1] = end;
+            x[i][2] = num;
+        }
+
+        dfs(0,0);
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+    }
+
+    public static void dfs(int sum, int now){
+        if(max < sum){
+            max = sum;
+        }
+        
+        for(int  i = 0 ;i<N;i++){
+            if(now <= x[i][0] && !visited[i]){
+                visited[i] = true;
+                dfs(sum + x[i][2], x[i][1]);
+                visited[i] = false;
+            }
+        }
+    }
+
+
+
+}
