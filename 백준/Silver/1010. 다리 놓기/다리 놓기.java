c2391e709f16b378ebaccaca9235ed754f84527e
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2024-08-28 17:27:20 +0900

    [Silver V] Title: 다리 놓기, Time: 128 ms, Memory: 14800 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -9,14 +9,13 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
+        dp = new int[30][30];
 
         for(int i = 0 ;i<N;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            dp = new int[b+1][a+1];
 
             bw.write(combi(b,a) + "\n");
             bw.flush();
@@ -25,12 +24,11 @@ public class Main {
     }
 
     static int combi(int n, int r){
-        if(dp[n][r] != 0){
+        if(dp[n][r] > 0){
             return dp[n][r];
         }
         if(n == r || r == 0){
-            dp[n][r] = 1;
-            return 1;
+            return dp[n][r] = 1;
         }
         return dp[n][r] = combi(n-1,r-1) + combi(n-1,r);
     }
