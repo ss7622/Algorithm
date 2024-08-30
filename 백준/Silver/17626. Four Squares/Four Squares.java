6c896c732c5c1d6ca889e9e8e63cd9b5cb1628ed
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2024-08-31 03:40:36 +0900

    [Silver III] Title: Four Squares, Time: 148 ms, Memory: 14576 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static int N=0,dp[];
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+
+        dp= new int[N+100];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+
+        bw.write("" + cur(N));
+        bw.flush();
+    }
+
+    static int cur(int n){
+        if(dp[n] == 0 && n != 0){
+            dp[n] = 50000;
+            for(int i=(int)Math.sqrt(n);i>=2;i--){
+                dp[n] = Math.min(cur(n - i*i), dp[n]);
+            }
+            dp[n]++;
+        }
+
+        return dp[n];
+
+    }
+}
