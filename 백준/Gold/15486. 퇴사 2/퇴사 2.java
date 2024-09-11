6a0e8b0ec3a64488cd321f05fa14f9e2a4f1f00b
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2024-09-11 16:06:10 +0900

    [Gold V] Title: 퇴사 2, Time: 652 ms, Memory: 316340 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -3,16 +3,15 @@ import java.util.*;
 
 
 public class Main {
-    static int max = 0, dp[], t[],p[],N;
     public static void main(String args[])throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        dp = new int[N];
-        t = new int[N];
-        p = new int[N];
+        int[] dp = new int[N];
+        int[] t = new int[N];
+        int[] p = new int[N];
 
         for(int  i = 0 ;i<N;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -20,26 +19,25 @@ public class Main {
             t[i] = Integer.parseInt(st.nextToken());
             p[i] = Integer.parseInt(st.nextToken());
         }
+        
+        if(t[N-1]==1){
+            dp[N-1] = p[N-1];
+        }
 
-        for(int  i = N-1 ;i>=0;i--){
+        for(int  i = N-2 ;i>=0;i--){
             if(i + t[i] <= N){
                 dp[i] = p[i];
                 if(i+t[i] < N){
                     dp[i] += dp[i+t[i]];
                 }
-                for(int j = i+1; j < i+t[i];j++){
-                    dp[i] = Math.max(dp[i],dp[j]);
-                }
+                dp[i] = Math.max(dp[i],dp[i+1]);
             }
             else{
-                if(i != N-1){
-                    dp[i] = dp[i+1];
-                }
+                dp[i] = dp[i+1];
             }
-            max = Math.max(max,dp[i]);
         }
 
-        bw.write(max + "");
+        bw.write(String.valueOf(dp[0]));
         bw.flush();
     }
 }
