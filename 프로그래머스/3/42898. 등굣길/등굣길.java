57a4191b8fb0c07ab93dd28fc132593a697a29da
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-01-16 04:01:24 +0900

    [level 3] Title: 등굣길, Time: 0.97 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] count = new int[n+1][m+1];
+        count[1][1] = 1;
+        
+        for(int[] pu : puddles){
+            count[pu[1]][pu[0]] = -1;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=m;j++){
+                if(count[i][j] == 0){
+                    if(count[i-1][j] == -1){
+                        count[i][j] = count[i][j-1];
+                    }
+                    else if(count[i][j-1] == -1){
+                        count[i][j] = count[i-1][j];
+                    }
+                    else if(count[i-1][j] == -1 && count[i][j-1] == -1){
+                        count[i][j] = -1;
+                    }
+                    else{
+                        count[i][j] = (count[i-1][j] + count[i][j-1]) % 1000000007;
+                    }
+                }
+            }
+        }
+        
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         System.out.print(count[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        return count[n][m];
+    }
+
+}
