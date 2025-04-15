fe6df990ec9fa751d1645f6d45104bbb40ce3f38
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-04-15 09:30:35 +0900

    [Silver I] Title: 종이 접기, Time: 128 ms, Memory: 16480 KB -BaekjoonHub

--- 백준/Silver/1802. 종이 접기/종이 접기.java
+++ 백준/Silver/1802. 종이 접기/종이 접기.java
@@ -1,58 +1,49 @@
-import java.util.Scanner;
-import java.util.Arrays;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    static boolean check = true;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static int devide(char[] x, int left, int right){
+        int T = Integer.parseInt(br.readLine());
 
-        int mid = (left+right) / 2 ;
+        for(int i=0;i<T;i++){
+            check = true;
+            char[] x = br.readLine().toCharArray();
+            int paper[] = new int[x.length];
 
-        if(right - left > 3) {
-            int check = devide(x, left, mid-1);
-            if (check == 0) {
-                return 0;
+            for(int j=0;j<x.length;j++){
+                paper[j]=Integer.parseInt(x[j]+"");
             }
-            check = devide(x, mid + 1, right);
-            if (check == 0) {
-                return 0;
-            }
-        }
 
-        for(int i=left, k=right;i<mid;i++,k--){
-            if(x[i] == x[k]){
-                return 0;
-            }
-        }
+            devide(paper,0,x.length-1);
 
-        return 1;
-
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int T = sc.nextInt();
-        sc.nextLine();
-        String x;
-
-        for(int i = 0;i<T;i++){
-            x = sc.nextLine();
-
-            int len = x.length();
-            char[] split = new char[len];
-
-            split = x.toCharArray();
-            int check = devide(split,0,len-1);
-
-            if(check == 1){
+            if(check){
                 System.out.println("YES");
-            }
-            else{
+            } else{
                 System.out.println("NO");
             }
-
-
         }
+    }
+
+    public static void devide(int[] x,int left,int right){
+
+        if(right - left < 2){
+            return;
+        }
+
+        int mid = (left + right) / 2;
+
+        for(int i=left,j=right;i<mid;i++,j--){
+            if(x[i]+x[j] != 1){
+                check = false;
+                return;
+            }
+        }
+
+        devide(x,left,mid-1);
+        devide(x,mid+1,right);
     }
 }
