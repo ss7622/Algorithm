8ddf108f8b4d56c7fde0624cfc44ec69e9eef995
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-04-22 18:22:00 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 16176 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.lang.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int[][] miro;
+    static boolean visited[][];
+    static int x[] = {-1,1,0,0};
+    static int y[] = {0,0,-1,1};
+    static int N, M, result = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        miro = new int[N][M];
+        visited = new boolean[N][M];
+
+        for(int i=0;i<N;i++){
+            String line = br.readLine();
+            for(int j=0;j<M;j++){
+                miro[i][j] = Integer.parseInt(line.charAt(j)+"");
+            }
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {0, 0});
+        bfs(1, queue);
+        System.out.println(result);
+    }
+
+    public static void bfs (int depth, Queue<int[]> queue){
+        if(result != 0){
+            return;
+        }
+        Queue<int[]> nextQueue = new LinkedList<>();
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            for(int i=0;i<4;i++){
+                int nx = cur[0]+x[i];
+                int ny = cur[1]+y[i];
+                if (ny == N-1 && nx == M-1){
+                    result = depth+1;
+                    return;
+                }
+                if(ny >= 0 && ny < N && nx >= 0 && nx < M && !visited[ny][nx] && miro[ny][nx] == 1){
+                    visited[ny][nx] = true;
+                    nextQueue.offer(new int[]{nx,ny});
+                }
+            }
+        }
+        bfs(depth+1,nextQueue);
+    }
+}
