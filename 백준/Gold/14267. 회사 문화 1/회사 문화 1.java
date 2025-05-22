38d2869cae20d35bc12fe081c80ea785cc5fe619
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-05-22 18:36:05 +0900

    [Gold IV] Title: 회사 문화 1, Time: 1376 ms, Memory: 62356 KB -BaekjoonHub

---
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int chingChanCnt = Integer.parseInt(st.nextToken());
+
+        int[] myUpper = new int[n];
+        int[] dp = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            myUpper[i] = Integer.parseInt(st.nextToken());
+        }
+        myUpper[0] = 0;
+
+        for(int i=0;i<chingChanCnt;i++){
+            st = new StringTokenizer(br.readLine());
+            int chingChanGet = Integer.parseInt(st.nextToken());
+            int chingChanVal = Integer.parseInt(st.nextToken());
+            dp[chingChanGet] += chingChanVal;
+        }
+
+        for(int i=1;i<=n;i++){
+            dp[i] += dp[myUpper[i-1]];
+        }
+
+        for(int i=1;i<=n;i++){
+            System.out.print(dp[i]+ " ");
+        }
+    }
+}
