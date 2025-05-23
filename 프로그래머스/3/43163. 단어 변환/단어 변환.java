c5650d6e48e8e418c53817a3739479ba3ef903e5
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-05-23 11:23:14 +0900

    [level 3] Title: 단어 변환, Time: 0.64 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,48 +1,50 @@
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+
 class Solution {
-        static HashSet<String> visited = new HashSet<>();
-        static int cnt = 0;
-
-        public int solution(String begin, String target, String[] words) {
-            Queue<String[]> queue = new LinkedList<>();
-            queue.offer(new String[] {begin, "0"});
-            bfs(queue, begin, target, words);
-            return cnt;
+    public int solution(String begin, String target, String[] wordss) {
+        Set<String> visited = new HashSet<>();
+        Queue<int[]> que = new LinkedList<>();
+        String[] words = new String[wordss.length+1];
+        words[0] = begin;
+        int flag = 1;
+        for(String word : wordss){
+            words[flag++] = word;
         }
-
-        public void bfs(Queue<String[]> queue, String begin, String target, String[] words) {
-
-            while (!queue.isEmpty()) {
-                String[] poll = queue.poll();
-
-                if(poll[0].equals(target)) {
-                    cnt = Integer.parseInt(poll[1]);
-                    return;
-                }
-
-                for (String word : words) {
-
-                    if (!visited.contains(word) && check(poll[0], word)) {
-                        visited.add(word);
-                        queue.add(new String[]{word, (Integer.parseInt(poll[1])+1) + ""});
-                    }
+        que.add(new int[] {0,0});
+        visited.add(begin);
+        
+        while(!que.isEmpty()){
+            int[] now = que.poll();
+            
+            if(words[now[0]].equals(target)){
+                return now[1];
+            }
+            
+            for(int i=0;i<words.length;i++){
+                if(!visited.contains(words[i]) && getChai(words[now[0]], words[i])){
+                    visited.add(words[i]);
+                    que.add(new int[] {i,now[1]+1});
                 }
             }
         }
-
-        public boolean check(String a, String b){
-            char[] aa = a.toCharArray();
-            char[] bb = b.toCharArray();
-            int cnt = 0;
-
-            for(int i=0;i<aa.length;i++){
-                if(aa[i]==bb[i]){
-                    cnt++;
-                }
-            }
-            return cnt == a.length() - 1;
-        }
+        return 0;
+        
     }
+    
+    public boolean getChai(String a, String b){
+        char[] a2 = a.toCharArray();
+        char[] b2 = b.toCharArray();
+        int sum = 0;
+        
+        for(int i=0;i<a2.length;i++){
+            if(a2[i] != b2[i]){
+                sum++;
+            }
+        }
+        
+        if(sum == 1){
+            return true;
+        }
+        return false;
+    }
+}
