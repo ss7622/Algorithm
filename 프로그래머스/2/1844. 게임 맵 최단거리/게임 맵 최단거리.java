1260c532eb062af117c09b2633af0c8d50b516e5
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-05-29 09:25:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.86 ms, Memory: 58.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,22 +1,21 @@
 import java.util.*;
+import java.lang.*;
 
 class Solution {
-    static boolean visited[][];
-    static int[] toY = {0,0,1,-1};
-    static int[] toX = {1,-1,0,0};
-    
     public int solution(int[][] maps) {
-        int yLength = maps.length;
-        int xLength = maps[0].length;
-        visited = new boolean[maps.length][maps[0].length];
-        visited[0][0] = true;
+        int[] toY = {0,0,1,-1};
+        int[] toX = {1,-1,0,0};
+        
+        boolean visited[][] = new boolean[maps.length][maps[0].length];
         Queue<int[]> que = new LinkedList<>();
+        
         que.add(new int[] {0,0,1});
+        visited[0][0] = true;
         
         while(!que.isEmpty()){
             int[] now = que.poll();
             
-            if(now[0] == yLength-1 && now[1] == xLength-1){
+            if(now[0] == maps.length-1 && now[1] == maps[0].length-1){
                 return now[2];
             }
             
@@ -24,15 +23,14 @@ class Solution {
                 int nextY = now[0] + toY[i];
                 int nextX = now[1] + toX[i];
                 
-                if(nextY >= 0 && nextY < yLength && nextX >= 0 && nextX < xLength
+                if(nextY >= 0 && nextY < maps.length && nextX >= 0 && nextX < maps[0].length
                   && !visited[nextY][nextX] && maps[nextY][nextX] == 1){
                     visited[nextY][nextX] = true;
-                    que.add(new int[] {nextY, nextX, now[2]+1});
+                    que.add(new int[] {nextY, nextX, now[2] + 1});
                 }
             }
         }
-        
-        
         return -1;
+        
     }
 }
