8368561b75edfdabd10617bc150128d844e5995e
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-05-30 09:35:51 +0900

    [level 3] Title: 단어 변환, Time: 0.56 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 83.9 MB, 시간: 0.69 ms
+메모리: 81.4 MB, 시간: 0.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 29일 09:45:34
+2025년 05월 30일 09:35:48
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,27 +2,24 @@ import java.util.*;
 import java.lang.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        Queue<String[]> que = new LinkedList<>();
         Set<String> visited = new HashSet<>();
+        Queue<String[]> que = new LinkedList<>();
         
-        que.add(new String[] {begin,"0"});
-        visited.add(begin);
+        que.add(new String[] {begin, "0"});
         
         while(!que.isEmpty()){
-            String now[] = que.poll();
-            
+            String[] now = que.poll();
             if(now[0].equals(target)){
                 return Integer.parseInt(now[1]);
             }
             
             for(int i=0;i<words.length;i++){
-                if(check(now[0], words[i]) && !visited.contains(words[i])){
+                if(!visited.contains(words[i]) && check(words[i], now[0])){
                     visited.add(words[i]);
-                    que.add(new String[] {words[i], String.valueOf(Integer.parseInt(now[1])+1)});
+                    que.add(new String[] {words[i],String.valueOf( Integer.parseInt(now[1]) + 1)});
                 }
             }
         }
-        
         
         return 0;
     }
@@ -31,12 +28,12 @@ class Solution {
         char[] a2 = a.toCharArray();
         char[] b2 = b.toCharArray();
         int cnt = 0;
-        
-        for(int i = 0;i<a2.length;i++){
+        for(int i=0;i<a2.length;i++){
             if(a2[i] == b2[i]){
                 cnt++;
             }
         }
+        
         if(cnt == a2.length-1){
             return true;
         }
