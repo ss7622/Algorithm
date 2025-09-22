edf8d8a8596e54848195229e397a0a4df10a40a9
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-09-22 13:51:54 +0900

    [level 3] Title: 단속카메라, Time: 11.71 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,26 +1,18 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
-import java.util.stream.Collectors;
-
-class Solution {
-    public static int solution(int[][] routes) {
-        // 첫 번째 원소 기준으로 오름차순 정렬
-        Arrays.sort(routes, (a, b) -> Integer.compare(a[0], b[0]));
-
-        int max = routes[0][1];
-        int cnt = 1;
-
-        for (int[] route : routes) {
-            if(max > route[1]){
-                max = route[1];
-            }
-            if (max < route[0]) {
-                cnt++;
-                max = route[1];
-            }
-        }
-
-        return cnt; // 예제에서는 반환값이 필요해서 임시로 0을 반환
-    }
-}
+	 class Solution {
+		public int solution(int[][] routes) {
+			Arrays.sort(routes, (o1, o2) -> o1[0] - o2[0]);
+			int cnt = 0;
+			int min = Integer.MAX_VALUE;
+			for (int[] route : routes) {
+				if(min < route[0]){
+					cnt++;
+					min = route[1];
+					continue;
+				}
+				min = Math.min(min, route[1]);
+			}
+			cnt++;
+			return cnt;
+		}
+	}
