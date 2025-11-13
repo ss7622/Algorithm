1afafd699ad4204559d3b1da88be5bcdf7a5e3f2
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2025-11-13 15:26:36 +0900

    [level 3] Title: 보행자 천국, Time: 158.91 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -4,18 +4,14 @@ class Solution {
     public int solution(int m, int n, int[][] cityMap) {
         int[][][] dp = new int[m][n][2];
         
-        // 1이 오른쪽 0이 아래
-        
         for(int i=0;i<n;i++){
             if(cityMap[0][i] == 1) break;
-            dp[0][i][0] = 0;
             dp[0][i][1] = 1;
         }
         
         for(int i=0;i<m;i++){
             if(cityMap[i][0] == 1) break;
             dp[i][0][0] = 1;
-            dp[i][0][1] = 0;
         }
         
         for(int i=1;i<m;i++){
@@ -33,14 +29,6 @@ class Solution {
                 }
             }
         }
-        
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 System.out.print(j + " : " + dp[i][j][0] + "," + dp[i][j][1] + " | ");
-//             }
-//             System.out.println();
-            
-//         }
         
         return (dp[m-1][n-1][0] + dp[m-1][n-1][1]) % MOD;
         
