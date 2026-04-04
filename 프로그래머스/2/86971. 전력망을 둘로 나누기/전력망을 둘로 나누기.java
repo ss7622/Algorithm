6d4d53f295cdac7c0c712c53e2d1148c948b3aa6
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2026-04-05 03:02:59 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.44 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,31 +1,33 @@
+import java.util.*;
 class Solution {
+    static int[] childCnt;
     static boolean[] visited;
-    static int[] child;
     public int solution(int n, int[][] wires) {
+        childCnt = new int[n+1];
         visited = new boolean[n+1];
-        child = new int[n+1];
-        visited[wires[0][0]] = true;
-        dfs(wires[0][0], wires);
+        visited[1] = true;
+        dfs(1, wires);
+        
         int min = Integer.MAX_VALUE;
         
-        for(int i=1;i<=n;i++){
-            min = Math.min(min,  Math.abs(  (n - child[i]) - child[i]       )        );
+        for(int i : childCnt){
+            min = Math.min(min, Math.abs((n - i) - i));
         }
         return min;
+        
     }
-    
     public int dfs(int now, int[][] wires){
-        int cnt = 0;
+        int sum = 0;
         for(int i=0;i<wires.length;i++){
-            if(wires[i][0] == now && !visited[wires[i][1]]){
+            if(now == wires[i][0] && !visited[wires[i][1]]){
                 visited[wires[i][1]] = true;
-                cnt += dfs(wires[i][1], wires);
+                sum += dfs(wires[i][1], wires);
             }
-            if(wires[i][1] == now && !visited[wires[i][0]]){
+            if(now == wires[i][1] && !visited[wires[i][0]]){
                 visited[wires[i][0]] = true;
-                cnt += dfs(wires[i][0], wires);
+                sum += dfs(wires[i][0], wires);
             }
         }
-        return child[now] = cnt+1;
+        return childCnt[now] = sum + 1;
     }
 }
