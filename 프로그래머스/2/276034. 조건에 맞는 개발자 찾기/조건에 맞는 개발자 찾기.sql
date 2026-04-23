bc20ce225a394c3eb8baae431d79c91c61e8563f
Junbeom Lee <63588899+ss7622@users.noreply.github.com>
2026-04-23 20:12:51 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,6 +1,4 @@
--- 코드를 작성해주세요
-select ID, EMAIL, FIRST_NAME, LAST_NAME
-from DEVELOPERS
-where SKILL_CODE & (select CODE from SKILLCODES where NAME = 'Python') > 0
-or SKILL_CODE & (select CODE from SKILLCODES where NAME = 'C#') > 0
-order by id
+select ID as ID, EMAIL, FIRST_NAME, LAST_NAME from DEVELOPERS
+where SKILL_CODE & (select CODE from SKILLCODES where NAME = 'C#') > 0 or
+SKILL_CODE & (select CODE from SKILLCODES where NAME = 'Python') > 0
+order by ID
